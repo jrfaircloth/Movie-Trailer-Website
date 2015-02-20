b3efecad22e02cd1d8fe48775ad685100bd467fa
jrfaircloth <jrfaircloth@gmail.com>
2015-02-20 00:25:57 -0500

    Revert the modal

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -56,7 +56,7 @@ main_page_head = '''
     </style>
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop', function (event) {
+        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
             // Remove the src so the player itself gets removed, as this is the only
             // reliable way to ensure the video stops playing in IE
             $("#trailer-video-container").empty();
